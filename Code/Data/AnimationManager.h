348bdc8a22acc60f9d01c87c0bde77a0d2ad05e0
Jan Dalski <jan.dalski@web.de>
2015-07-10 15:02:21 +0200

    Basic structures refined. Next step: Initialization / conversion.

--- Code/Data/AnimationManager.h
+++ Code/Data/AnimationManager.h
@@ -8,15 +8,37 @@
 using namespace std;
 
 
-struct Bone_t {};
-struct Keyframe {};
-
-struct Animation {
-  char* id;
-  float lastFrame;
+/** Bone element. A bone hierarchy is used to express a model's skeleton. */
+struct Bone_t {
+  int ID;            // Sequential bone identifier to access transformation arrays.  
+  char* Name;        // Bone name, maybe useful for text-based storage.
+  Bone_t* Parent;    // Parent bone.
+  Bone_t* Children;  // Child bones following in the hierarchy below this one.
+  long* VertexIdx;   // Array of vertices (indices) that are associated to this bone.
+  uint NrChildren;   // Number of child bones.
+  uint NrVertices;   // Number of vertex indices.
+  //TODO Maybe min/max extents and bone position?
 };
 
 
+/** Transformation specification. Defines modifications to apply to a bone and its subnodes. */
+struct Transformation {
+  uint Frame;          // The frame this transformation belongs to. 
+  Bone_t* Bone;        // Parent bone. 
+  Float3 Translation;  //| Transformation
+  Float4 Rotation;     //| directives to
+  Float3 Scaling;      //| apply to bone.
+  //TODO Flags to signalize used/unused directives.
+};
+
+
+/** Animation structure. Consists of a number of transformation rules to apply. */
+struct Animation {
+  char* Name;                       // Animation identifier.
+  uint FrameCount;                  // Length of animation (number of frames). 
+  int* NrTransformations;           // Number of transformations per bone.
+  Transformation** Transformations; // [Bone][Transformation] double array pointer.
+};
 
 
 /** Manages animations and interpolation for a model. */
@@ -26,10 +48,9 @@ private:
   Mesh* _baseMesh;                     // Model base mesh when no animation is applied.
   Bone_t* _skeleton;                   // The model's skeleton.
   map<string, Animation*> _animations; // Available animation definitions.
-
-  Animation* _curAnim = NULL;   // Animation in use.
-  Mesh* _curMesh = NULL;        // Mesh copy for applied transformations.
-  float _curFrame = 0;          // Counter for currently displayed frame.
+  Animation* _curAnim = NULL;          // Animation in use.
+  Mesh* _curMesh = NULL;               // Mesh copy for applied transformations.
+  float _curFrame = 0;                 // Counter for currently displayed frame.
 
 
 public:
@@ -62,7 +83,7 @@ public:
   /** Add a new animation to the set.
   * @param anim Animation structure. */
   void AddAnimation(Animation* anim) {
-    string str(anim->id);
+    string str(anim->Name);
     _animations[str] = anim;
   }
 
@@ -81,7 +102,7 @@ public:
 
     // Animation found. Check start frame.
     else {
-      if (startFrame > _animations[anim]->lastFrame) { // Frame interval exceeded!
+      if (startFrame > _animations[anim]->FrameCount) { // Frame interval exceeded!
         printf("[AnimationManager] Error setting animation '%s': Start frame '%d' out of bounds!\n", animId);
         _curAnim = NULL;
         return;
@@ -105,8 +126,8 @@ public:
 
       // Update the frame pointer and adjust it, if it exceeds boundaries.
       _curFrame += frameskip;
-      if (_curFrame > _curAnim->lastFrame) _curFrame = _curFrame - _curAnim->lastFrame;
-      else if (_curFrame < 0)              _curFrame = _curAnim->lastFrame + _curFrame;
+      if (_curFrame > _curAnim->FrameCount) _curFrame = _curFrame - _curAnim->FrameCount;
+      else if (_curFrame < 0)              _curFrame = _curAnim->FrameCount + _curFrame;
 
       //TODO
 
